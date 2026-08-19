2a3b1101fa40c68741137ce889a400982c19bd26
Amitchitkara999 <amitchitkara999@gmail.com>
2026-08-19 10:26:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
                         <div class="col-xl-9 col-lg-9 col-md-10">
                             <div class="hero__caption">
                                 <span data-animation="fadeInLeft" data-delay="0.1s">find your gym</span>
-                                <h1 data-animation="fadeInLeft" data-delay="0.4s">gym with us </h1>
+                                <h1 data-animation="fadeInLeft" data-delay="0.4s">gym  us </h1>
                                 <a href="gym.php" class="border-btn hero-btn" data-animation="fadeInLeft" data-delay="0.8s">Gym</a>
                             </div>
                         </div>
